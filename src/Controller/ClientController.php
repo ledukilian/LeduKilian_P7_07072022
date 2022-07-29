0006b43dd9a249e5f5edde7e3ddf69ee7ec64e46
ledukilian <ledu.kilian@gmail.com>
2022-07-29 10:13:35 +0200

    Added client details route

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -16,19 +16,19 @@ use Symfony\Component\Serializer\Serializer;
 class ClientController extends AbstractController
 {
     /**
-     * @Route("/company/{id}/clients/", name="show_clients")
+     * @Route("/company/{company_id}/clients/", name="show_clients")
      * @param ManagerRegistry $doctrine
      * @param int             $id
      * @return Response
      */
-    public function showClients(ManagerRegistry $doctrine, int $id): Response
+    public function showClients(ManagerRegistry $doctrine, int $company_id): Response
     {
         /* Get company */
         $company = $doctrine
             ->getRepository(Company::class)
             ->findBy(
                 [
-                    'id' => $id
+                    'id' => $company_id
                 ]
             );
 
@@ -54,6 +54,53 @@ class ClientController extends AbstractController
             return $this->json([
                 'success' => true,
                 'clients' => $clients
+            ], 200, [], []);
+        } else {
+            return $this->json([], 204, [], []);
+        }
+    }
+
+
+    /**
+     * @Route("/company/{company_id}/clients/{client_id}/", name="show_client")
+     * @param ManagerRegistry $doctrine
+     * @param int             $id
+     * @return Response
+     */
+    public function showClient(ManagerRegistry $doctrine, int $company_id, int $client_id): Response
+    {
+        /* Get company */
+        $company = $doctrine
+            ->getRepository(Company::class)
+            ->findBy(
+                [
+                    'id' => $company_id
+                ]
+            );
+
+        /* Get all clients */
+        $client = $doctrine
+            ->getRepository(Client::class)
+            ->findBy(
+                [
+                    'company' => $company,
+                    'id' => $client_id
+                ]
+            );
+
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $client = $serializer->normalize($client, null, [
+            AbstractNormalizer::ATTRIBUTES => ['id', 'email', 'firstname', 'lastname', 'company' => ['email', 'name']]
+        ]);
+
+        if (sizeof($client)>0) {
+            return $this->json([
+                'success' => true,
+                'client' => $client
             ], 200, [], []);
         } else {
             return $this->json([], 204, [], []);
